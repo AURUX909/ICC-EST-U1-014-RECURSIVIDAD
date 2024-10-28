cff2ed8c1d6873c5caf536a9c2bdab99afc272e0
Renato <bryanans001@gmail.com>
2024-10-28 13:01:17 -0500

    Finalizacion de la practica

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Recursividad rec = new Recursividad();
+        int result = rec.factorial(5);
+        System.out.println(result);
+        System.out.println(rec.sumaConsecutuvas(5));
+        System.out.println(rec.potencia(2, 3));
+        System.out.println(rec.digital(456));
+    }
+}
